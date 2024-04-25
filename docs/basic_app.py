d93a37440419f7adb068b1bb3af8f9dad04be0d3
Berke Argın <berke.argin98@gmail.com>
2024-04-25 19:16:12 +0200

    Create basic_app.py

---
+++ docs/basic_app.py
@@ -0,0 +1,25 @@
+from http.server import SimpleHTTPRequestHandler, HTTPServer
+import urllib.parse
+
+class CustomHTTPRequestHandler(SimpleHTTPRequestHandler):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, directory="./", **kwargs)
+
+    def translate_path(self, path):
+        # Add prefix to the URL path
+        path = super().translate_path(path)
+        components = path.split('/')
+        components.pop(1)
+        #components.insert(1, 'project-2024-datatouille')
+        modified_path = '/'.join(components)
+        return modified_path
+
+def run(server_class=HTTPServer, handler_class=CustomHTTPRequestHandler, port=5000):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    print(f"Starting server on port {port}...")
+    httpd.serve_forever()
+
+if __name__ == "__main__":
+    run()
+
